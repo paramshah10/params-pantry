552b53243f7832a8296ec254ff58757a4ff7e304
Param Shah <paramshah09@gmail.com>
2022-01-18 11:35:16 -0800

    feat: background image and landing text; change how layout is done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ['FuturaPT', 'helvetica', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],

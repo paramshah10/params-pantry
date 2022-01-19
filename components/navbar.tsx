66e60c1a99140feb28476df2e252a8bd3217567f
Param Shah <paramshah09@gmail.com>
2022-01-18 16:06:34 -0800

    chore: background image and header image changes, navbar transition from transparent to opaque

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 
 interface Page {
@@ -13,6 +13,16 @@ const Navbar = () => {
   const handleClick = () => {
     setMenuActive(!menuActive);
   };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 80)
+        setOpaqueNavbar(true);
+      else
+        setOpaqueNavbar(false);
+    }
+    window.addEventListener('scroll', handleScroll);
+  }, []);
   
   const pages: Page[] = [
     {
@@ -30,50 +40,48 @@ const Navbar = () => {
   ]
 
   return (
-    <div className="bg-transparent">
-      <nav className={`fixed w-full flex items-center flex-wrap ${opaqueNavbar ? 'bg-slate-900' : ''} py-3 px-8`}>
-        <Link href='/'>
-          <a className='inline-flex items-center p-2 mr-4 '>
-            <img className='fill-current text-white h-8 w-8 mr-6' src='/apple-touch-icon.png' alt="Param's Pantry Logo"/>
-            <span className='text-xl text-white font-bold tracking-wide'>
-              {'Param\'s Pantry'}
-            </span>
-          </a>
-        </Link>
-        {/* Hamburger menu for smaller devices */}
-        <button className=' inline-flex p-3 hover:bg-green-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none'
-          onClick={handleClick}
+    <nav className={`fixed w-full flex items-center flex-wrap ${opaqueNavbar ? 'bg-black' : ''} py-3 px-8`}>
+      <Link href='/'>
+        <a className='inline-flex items-center p-2 mr-4 '>
+          <img className='fill-current text-white h-8 w-8 mr-6' src='/apple-touch-icon.png' alt="Param's Pantry Logo"/>
+          <span className='text-xl text-white font-bold tracking-wide'>
+            {'Param\'s Pantry'}
+          </span>
+        </a>
+      </Link>
+      {/* Hamburger menu for smaller devices */}
+      <button className=' inline-flex p-3 rounded lg:hidden text-white ml-auto hover:text-white outline-none'
+        onClick={handleClick}
+      >
+        <svg
+          className='w-6 h-6 hover:opacity-70'
+          fill='none'
+          stroke='currentColor'
+          viewBox='0 0 24 24'
+          xmlns='http://www.w3.org/2000/svg'
         >
-          <svg
-            className='w-6 h-6'
-            fill='none'
-            stroke='currentColor'
-            viewBox='0 0 24 24'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth={2}
-              d='M4 6h16M4 12h16M4 18h16'
-            />
-          </svg>
-        </button>
-        <div className={`${
-            menuActive ? '' : 'hidden'
-          }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}>
-          <div className='z-50 lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
-            {pages.map(page =>
-              <Link key={page.title} href={page.link}>
-                <a className='lg:inline-flex lg:w-auto w-full px-6 lg:py-2 py-4 rounded text-white font-bold items-center justify-center hover:text-slate-400 lowercase'>
-                  {page.title}
-                </a>
-              </Link>
-            )}
-          </div>
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth={2}
+            d='M4 6h16M4 12h16M4 18h16'
+          />
+        </svg>
+      </button>
+      <div className={`${
+          menuActive ? 'bg-black ease-in duration-500 will-change-transform' : 'hidden'
+        } w-screen lg:inline-flex lg:flex-grow lg:w-auto`}>
+        <div className='z-50 lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
+          {pages.map(page =>
+            <Link key={page.title} href={page.link}>
+              <a className='lg:inline-flex lg:w-auto w-full px-6 lg:py-2 py-4 rounded text-white font-bold items-center justify-center hover:text-slate-400 lowercase'>
+                {page.title}
+              </a>
+            </Link>
+          )}
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   )
 }
 

20680e6b0173a07d4c4711a4453185b7855bcbd7
Param Shah <paramshah09@gmail.com>
2022-01-18 16:06:34 -0800

    chore: background image and header image changes, navbar transition from transparent to opaque

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,23 +4,16 @@ import React from 'react';
 const Home: NextPage = () => {
   return (
     <>
-      <div className="flex flex-col justify-end items-center x-0 y-0">
-        <div className='-z-50 relative w-screen h-screen'>
-          <div className={`mx-0 my-0 overflow-hidden h-screen w-screen absolute`}
-            style={{
-              background: "url('/assets/pizza.jfif') no-repeat center center fixed",
-              backgroundSize: 'cover',
-              WebkitBackgroundSize: 'cover'
-            }}
-          >
-          </div>
-        </div>
-
-        <div className="-mt-96">
-          <h1 className="leading5 text-8xl font-bold text-white">
-            Fasting is overrated.
-          </h1>
-        </div>
+      <div className='-z-50 flex flex-col justify-center items-center relative w-screen h-screen'>
+        <div className={
+          `z-10 mx-0 my-0 overflow-hidden h-screen w-screen absolute 
+          bg-[url('../public/assets/pizza.jfif')] bg-no-repeat bg-center bg-fixed bg-cover`
+        }
+          // Bg image properties taken from https://css-tricks.com/perfect-full-page-background-image/
+        />
+        <h1 className="z-30 leading5 text-8xl font-bold text-white cursor-text">
+          Fasting is overrated.
+        </h1>
       </div>
       <main className="px-8 py-8 min-h-screen py-16 flex flex-1 flex-col justify-center items-center">
         <h1 className="m-0 leading-5 text-6xl">

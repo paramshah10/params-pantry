0d246f5be8a718cadc4f41f2f99c3be9d4347461
Param Shah <shparam@amazon.com>
2022-04-17 14:28:49 -0700

    fix: fix recipe title line height and remove showEditButton property

--- website/pages/recipe/[recipe].tsx
+++ website/pages/recipe/[recipe].tsx
@@ -16,7 +16,6 @@ export default function RecipePage(props: RecipePageProps) {
 
   const [recipeData, setRecipeData] = useState<Recipe | undefined>(undefined);
   const imageURL = 'https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80';
-  const [showEditButton, setShowEditButton] = useState(false);
 
   const fetchRecipe = async (name: string) => {
     const res = await window.fetch('/api/fetch-recipe', {
@@ -42,8 +41,6 @@ export default function RecipePage(props: RecipePageProps) {
   return (
     <>
       <div className='flex flex-col justify-center items-center relative w-full h-[75vh] mb-32'
-        onMouseEnter={_ => setShowEditButton(true)}
-        onMouseLeave={_ => setShowEditButton(false)}
       >
         <div className={
           `-z-50 mx-0 my-0 overflow-hidden h-screen w-full absolute 
@@ -52,12 +49,12 @@ export default function RecipePage(props: RecipePageProps) {
         style={{backgroundImage: `url('${recipeData?.image}')`}}
         // Bg image properties taken from https://css-tricks.com/perfect-full-page-background-image/
         />
-        <h1 className="z-30 lg:leading-5 lg:text-8xl md:text-7xl text-6xl text-center font-bold text-white cursor-text">
+        <h1 className="z-30 lg:text-8xl md:text-7xl text-6xl text-center font-bold text-white cursor-text">
           {recipeData?.name}
         </h1>
         {
           isAuthenticated ?
-            <div className='absolute z-30 right-24 top-24' style={showEditButton ? { display: 'block' } : { display: 'none' }}>
+            <div className='absolute z-30 right-24 top-24'>
               <EditButton type="photo" />
             </div>
             : <></>
@@ -68,12 +65,6 @@ export default function RecipePage(props: RecipePageProps) {
           <TextEditor editorContent={recipeData?.content}/>
         </div>
       </div>
-      {/*
-      <div className="px-8 min-h-screen py-8 flex flex-col items-center text-center">
-        <h1 className="my-24 lg:text-7xl md:text-6xl text-5xl leading-snug">
-          Welcome to {"Param's"} Pantry!
-        </h1>
-      </div> */}
     </>
   );
 }
